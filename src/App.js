0608ee717c63cb0c7abe7d54acb768dc1a7a29a2
mondirhallouli <halloulim@gmail.com>
2022-03-01 14:17:13 +0100

    removed some link element styles and added the mobile menu component to the App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import HeaderComponent from "./components/header/Header";
+import MenuComponent from "./components/header/MobileMenu";
 
 function App() {
+  // opening and closing mobile menu by checking the openMenu state
+  const [openMenu, setOpenMenu] = useState(false);
+  // open/close handler function
+  const handleMenu = () => {
+    openMenu ? setOpenMenu(false) : setOpenMenu(true);
+  };
+
   return (
     <div>
-      <HeaderComponent />
+      <HeaderComponent openMenu={handleMenu} />
+      <MenuComponent open={openMenu} closeMenu={handleMenu} />
     </div>
   );
 }
