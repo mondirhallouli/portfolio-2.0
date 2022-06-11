f224add8262706b98c9192c773f7bcc76696801a
mondirhallouli <halloulim@gmail.com>
2022-06-11 16:43:34 +0100

    added new project cart to projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -4,35 +4,56 @@ import {
   ProjectDesc,
   ProjectSkills,
   SkillTag,
-} from './styledComponents'
-import { SectionTitle } from '../layout';
-import ogbbThumb from '../../assets/images/ogbball-thumb.png'
-import devtaskThumb from '../../assets/images/devtask-thumb.png'
-import sweetThumb from '../../assets/images/sweet-&-cool-thumb.png'
-import CardComponent from './Card'
+} from "./styledComponents";
+import { SectionTitle } from "../layout";
+import ogbbThumb from "../../assets/images/ogbball-thumb.png";
+import devtaskThumb from "../../assets/images/devtask-thumb.png";
+import sweetThumb from "../../assets/images/sweet-&-cool-thumb.png";
+import eshopThumb from "../../assets/images/eshop-thumb.png";
+import CardComponent from "./Card";
 
 function ProjectsComponent() {
+  // Eshop ecommerce URLs
+  const eshopDemo = "https://eshop.mondirhallouli.com/";
+  const eshopCode = "https://github.com/mondirhallouli/eshop-ecommerce";
   // OG Bball project URLs for code and live version
-  const ogbballDemo = 'https://ogbball.mondirhallouli.com/';
-  const ogbballCode = 'https://github.com/mondirhallouli/ogbball-site';
+  const ogbballDemo = "https://ogbball.mondirhallouli.com/";
+  const ogbballCode = "https://github.com/mondirhallouli/ogbball-site";
   // DevTask project URLs
-  const devtaskDemo = 'https://devtask.mondirhallouli.com/';
-  const devtaskCode = 'https://github.com/mondirhallouli/devtask-manager';
+  const devtaskDemo = "https://devtask.mondirhallouli.com/";
+  const devtaskCode = "https://github.com/mondirhallouli/devtask-manager";
   // Sweet & Cool project URLs
-  const sncDemo = 'https://icecream.mondirhallouli.com/';
-  const sncCode = 'https://github.com/mondirhallouli/ice-cream-website';
+  const sncDemo = "https://icecream.mondirhallouli.com/";
+  const sncCode = "https://github.com/mondirhallouli/ice-cream-website";
 
   return (
     <ProjectsContainer>
       <SectionTitle>Projects</SectionTitle>
       {/* 1st project */}
+      <CardComponent img={eshopThumb} demoLink={eshopDemo} codeLink={eshopCode}>
+        <ProjectTitle>Eshop ecommerce</ProjectTitle>
+        <ProjectDesc>
+          Eshop is a minimal design of a fictional ecommerce business's
+          frontend.
+        </ProjectDesc>
+        <ProjectSkills>
+          <SkillTag>React</SkillTag>
+          <SkillTag>Fetch API</SkillTag>
+          <SkillTag>CSS</SkillTag>
+          <SkillTag>Redux</SkillTag>
+        </ProjectSkills>
+      </CardComponent>
+
+      {/* 2nd project */}
       <CardComponent
         img={ogbbThumb}
         demoLink={ogbballDemo}
-        codeLink={ogbballCode}>
+        codeLink={ogbballCode}
+      >
         <ProjectTitle>Original Basketball</ProjectTitle>
         <ProjectDesc>
-          Original Basketball is a concept of a fictional basketball coaching business with a YT viewer base.
+          Original Basketball is a concept of a fictional basketball coaching
+          business with a YT viewer base.
         </ProjectDesc>
         <ProjectSkills>
           <SkillTag>Figma</SkillTag>
@@ -43,14 +64,16 @@ function ProjectsComponent() {
         </ProjectSkills>
       </CardComponent>
 
-      {/* 2nd project */}
+      {/* 3rd project */}
       <CardComponent
         img={devtaskThumb}
         demoLink={devtaskDemo}
-        codeLink={devtaskCode}>
+        codeLink={devtaskCode}
+      >
         <ProjectTitle>Devtask Manager</ProjectTitle>
         <ProjectDesc>
-          DEVTASK is a react app that allows the user to manage their workflow through a simple task management system.
+          DEVTASK is a react app that allows the user to manage their workflow
+          through a simple task management system.
         </ProjectDesc>
         <ProjectSkills>
           <SkillTag>Figma</SkillTag>
@@ -62,14 +85,12 @@ function ProjectsComponent() {
         </ProjectSkills>
       </CardComponent>
 
-      {/* 3rd project */}
-      <CardComponent
-        img={sweetThumb}
-        demoLink={sncDemo}
-        codeLink={sncCode}>
+      {/* 4th project */}
+      <CardComponent img={sweetThumb} demoLink={sncDemo} codeLink={sncCode}>
         <ProjectTitle>Sweet &amp; Cool website</ProjectTitle>
         <ProjectDesc>
-          Cool & Sweet is a fictional small business, designed and coded with both business and clients in mind.
+          Cool & Sweet is a fictional small business, designed and coded with
+          both business and clients in mind.
         </ProjectDesc>
         <ProjectSkills>
           <SkillTag>Figma</SkillTag>
@@ -80,7 +101,7 @@ function ProjectsComponent() {
         </ProjectSkills>
       </CardComponent>
     </ProjectsContainer>
-  )
+  );
 }
 
-export default ProjectsComponent
+export default ProjectsComponent;
