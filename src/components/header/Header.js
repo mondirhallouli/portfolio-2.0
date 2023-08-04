52a543aed12a24915522c4a18f62d26042013281
mondirhallouli <halloulim@gmail.com>
2023-08-04 18:31:02 +0100

    updated the resume link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,80 +1,80 @@
 import { useState } from 'react'
 import logo from '../../assets/images/logo.svg'
 import {
-  Header,
-  Nav,
-  NavContainer,
-  Logo,
-  MenuBtn,
-  MenuList,
-  MenuItem,
-  MenuLink,
-  HeaderTitle
+	Header,
+	Nav,
+	NavContainer,
+	Logo,
+	MenuBtn,
+	MenuList,
+	MenuItem,
+	MenuLink,
+	HeaderTitle
 } from './layout'
 
 function HeaderComponent({ openMenu }) {
 
-  // state holder to change the navbar color when scrolling
-  const [navColor, setNavColor] = useState(false);
+	// state holder to change the navbar color when scrolling
+	const [navColor, setNavColor] = useState(false);
 
-  // handler function to change state on scroll
-  const changeBg = () => {
-    if (window.scrollY > 50) {
-      setNavColor(true);
-    } else {
-      setNavColor(false);
-    }
-  };
+	// handler function to change state on scroll
+	const changeBg = () => {
+		if (window.scrollY > 50) {
+			setNavColor(true);
+		} else {
+			setNavColor(false);
+		}
+	};
 
-  // event listener on the window object to watch for scrolling
-  window.addEventListener('scroll', changeBg);
+	// event listener on the window object to watch for scrolling
+	window.addEventListener('scroll', changeBg);
 
-  return (
-    <Header>
-      {/* main navbar container given the setBg attribute to change navbar background when user scrolls */}
-      <Nav setBg={navColor}>
-        {/* inner navbar container to center content */}
-        <NavContainer>
-          <Logo href='/'>
-            <img src={logo} alt="mondir hallouli's logo" />
-          </Logo>
+	return (
+		<Header>
+			{/* main navbar container given the setBg attribute to change navbar background when user scrolls */}
+			<Nav setBg={navColor}>
+				{/* inner navbar container to center content */}
+				<NavContainer>
+					<Logo href='/'>
+						<img src={logo} alt="mondir hallouli's logo" />
+					</Logo>
 
-          {/* desktop menu list */}
-          <MenuList>
-            <MenuItem>
-              <MenuLink
-                onClick={() => document.querySelectorAll('section')[1].scrollIntoView(true)}
-              >
-                Projects
-              </MenuLink>
-            </MenuItem>
-            <MenuItem>
-              <MenuLink
-                onClick={() => document.querySelectorAll('section')[2].scrollIntoView(true)}
-              >
-                Contact
-              </MenuLink>
-            </MenuItem>
-            <MenuItem>
-              <MenuLink href='https://docs.google.com/document/d/1sZrVOGfdLUf4zovxLtu-4pMDbJLYaN2TSmhBhJWnQKg/edit?usp=sharing' target="_blank">Resume</MenuLink>
-            </MenuItem>
-          </MenuList>
+					{/* desktop menu list */}
+					<MenuList>
+						<MenuItem>
+							<MenuLink
+								onClick={() => document.querySelectorAll('section')[1].scrollIntoView(true)}
+							>
+								Projects
+							</MenuLink>
+						</MenuItem>
+						<MenuItem>
+							<MenuLink
+								onClick={() => document.querySelectorAll('section')[2].scrollIntoView(true)}
+							>
+								Contact
+							</MenuLink>
+						</MenuItem>
+						<MenuItem>
+							<MenuLink href='https://docs.google.com/document/d/1xp3Bc2UYjBRifNcpx75odMDUDGWtmNnjpEtthdUOWzQ/edit?usp=sharing' target="_blank">Resume</MenuLink>
+						</MenuItem>
+					</MenuList>
 
-          {/* mobile menu button */}
-          <MenuBtn onClick={openMenu}>
-            <span />
-            <span />
-            <span />
-          </MenuBtn>
-        </NavContainer>
-      </Nav>
+					{/* mobile menu button */}
+					<MenuBtn onClick={openMenu}>
+						<span />
+						<span />
+						<span />
+					</MenuBtn>
+				</NavContainer>
+			</Nav>
 
-      {/* header intro text */}
-      <HeaderTitle>
-        Hello! I'm Mondir, a React developer
-      </HeaderTitle>
-    </Header>
-  )
+			{/* header intro text */}
+			<HeaderTitle>
+				Hello! I'm Mondir, a React developer
+			</HeaderTitle>
+		</Header>
+	)
 }
 
 export default HeaderComponent
--- src/components/header/MobileMenu.js
+++ src/components/header/MobileMenu.js
@@ -1,42 +1,42 @@
 import {
-  Menu,
-  Close,
-  LinksWrapper,
-  MobileLink
+	Menu,
+	Close,
+	LinksWrapper,
+	MobileLink
 } from "./layout"
 
 function MenuComponent({ open, closeMenu }) {
-  // scroll to section handler function
-  const scrollToSection = (section) => {
-    section.scrollIntoView(true);
-    // close the menu after scrolling
-    closeMenu();
-  }
+	// scroll to section handler function
+	const scrollToSection = (section) => {
+		section.scrollIntoView(true);
+		// close the menu after scrolling
+		closeMenu();
+	}
 
-  return (
-    // Menu given the open attribute to check whether to show or hide itself
-    <Menu open={open}>
-      {/* close button with close function when clicked */}
-      <Close onClick={closeMenu}>
-        <span />
-        <span />
-      </Close>
-      {/* Mobile menu wrapper */}
-      <LinksWrapper>
-        <MobileLink
-          onClick={() => scrollToSection(document.querySelectorAll('section')[1])}
-        >
-          Projects
-        </MobileLink>
-        <MobileLink
-          onClick={() => scrollToSection(document.querySelectorAll('section')[2])}
-        >
-          Contact
-        </MobileLink>
-        <MobileLink href='https://docs.google.com/document/d/1iqAk4MQNinumHgRGhYil4lRFs47rAdp3QzKCTTIYia0/edit' target="_blank">Resume</MobileLink>
-      </LinksWrapper>
-    </Menu>
-  )
+	return (
+		// Menu given the open attribute to check whether to show or hide itself
+		<Menu open={open}>
+			{/* close button with close function when clicked */}
+			<Close onClick={closeMenu}>
+				<span />
+				<span />
+			</Close>
+			{/* Mobile menu wrapper */}
+			<LinksWrapper>
+				<MobileLink
+					onClick={() => scrollToSection(document.querySelectorAll('section')[1])}
+				>
+					Projects
+				</MobileLink>
+				<MobileLink
+					onClick={() => scrollToSection(document.querySelectorAll('section')[2])}
+				>
+					Contact
+				</MobileLink>
+				<MobileLink href='https://docs.google.com/document/d/1xp3Bc2UYjBRifNcpx75odMDUDGWtmNnjpEtthdUOWzQ/edit?usp=sharing' target="_blank">Resume</MobileLink>
+			</LinksWrapper>
+		</Menu>
+	)
 }
 
 export default MenuComponent
