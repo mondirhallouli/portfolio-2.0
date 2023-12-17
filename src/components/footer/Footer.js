713316c1d984a157e632e7f51571e8966128f044
mondirhallouli <halloulim@gmail.com>
2023-12-17 19:06:37 +0100

    updated portfolio

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,27 +1,27 @@
 import {
-  FooterContainer,
-  FooterWrapper,
-  FooterLogo,
-  FooterYear,
-  BackToTop
+    FooterContainer,
+    FooterWrapper,
+    FooterLogo,
+    FooterYear,
+    BackToTop
 } from './styledComponents'
 import btt from '../../assets/images/btt.svg'
 import logo from '../../assets/images/logo.svg'
 
 function FooterComponent() {
-  return (
-    <FooterContainer>
-      <FooterWrapper>
-        <FooterLogo href="/">
-          <img src={logo} alt="mondir hallouli's logo" />
-        </FooterLogo>
-        <FooterYear>&copy; 2022</FooterYear>
-        <BackToTop onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}>
-          <img src={btt} alt="back to top icon" />
-        </BackToTop>
-      </FooterWrapper>
-    </FooterContainer>
-  )
+    return (
+        <FooterContainer>
+            <FooterWrapper>
+                <FooterLogo href="/">
+                    <img src={logo} alt="mondir hallouli's logo" />
+                </FooterLogo>
+                <FooterYear>&copy; 2023</FooterYear>
+                <BackToTop onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}>
+                    <img src={btt} alt="back to top icon" />
+                </BackToTop>
+            </FooterWrapper>
+        </FooterContainer>
+    )
 }
 
 export default FooterComponent
